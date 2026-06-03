a3664377e08185827d445ee453d6deb79bca546e
Eugene Kozlov <kozlovea8@gmail.com>
2026-06-03 13:59:10 +0300

    Add solution for log-levels

---
+++ java/log-levels/src/main/java/LogLevels.java
@@ -0,0 +1,16 @@
+public class LogLevels {
+
+    public static String message(String logLine) {
+        return logLine.split(": ")[1].strip();
+    }
+
+    public static String logLevel(String logLine) {
+        String levelRaw = logLine.split(": ")[0];
+
+        return levelRaw.substring(1, levelRaw.length() - 1).toLowerCase().strip();
+    }
+
+    public static String reformat(String logLine) {
+        return String.format("%s (%s)", message(logLine), logLevel(logLine));
+    }
+}
